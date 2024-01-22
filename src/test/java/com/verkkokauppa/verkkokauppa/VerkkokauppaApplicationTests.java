860d5eb5f804e0b8e6816e5f1829e8d8e72e9930
mikkomakel <mikko.makela@seamk.fi>
2024-01-22 13:38:33 +0200

    spring boot projektin alku

---
+++ src/test/java/com/verkkokauppa/verkkokauppa/VerkkokauppaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verkkokauppa.verkkokauppa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerkkokauppaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
