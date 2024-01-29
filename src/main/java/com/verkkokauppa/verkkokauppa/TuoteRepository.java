2b8ddf0d765a35a06258f43297a15ed2556e6922
mikkomakel <mikko.makela@seamk.fi>
2024-01-29 13:54:08 +0200

    Lisätty Controller ja Repository tuotteelle

---
+++ src/main/java/com/verkkokauppa/verkkokauppa/TuoteRepository.java
@@ -0,0 +1,7 @@
+package com.verkkokauppa.verkkokauppa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TuoteRepository extends JpaRepository<Tuote, Long>{
+    
+}
